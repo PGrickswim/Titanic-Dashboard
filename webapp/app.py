8febe824ab6c95769fbac3a3d9d0e6c889969f82
Christine K <chknight@umich.edu>
2022-05-31 02:51:29 -0400

    Reverting app.py.

--- webapp/app.py
+++ webapp/app.py
@@ -6,9 +6,9 @@ import requests
 from random import choice
 from sklearn.preprocessing import StandardScaler
 
-# import sys
-# sys.path.insert(0, '..')
-# from Notebooks import config
+import sys
+sys.path.insert(0, '..')
+from Notebooks import config
 
 # Use pickle to load in the pre-trained model and fitted scaler.
 with open(f'model/titanic_model.pkl', 'rb') as f:
@@ -160,7 +160,7 @@ def get_db_connection():
     conn = psycopg2.connect(host='localhost',
                             database='titanic_project',
                             user='postgres',
-                            password='paradise22')
+                            password=config.db_password)
     return conn
 
 # API route that gets all passengers from database
